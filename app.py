9d645c6610cf04332006ab1a4a1fe27dfa43b0d1
Mohd Jibran <jibransheikh314@gmail.com>
2023-04-05 14:08:06 +0530

    spelling check

--- app.py
+++ app.py
@@ -85,8 +85,8 @@ def car():
     # except:
     #     return render_template("404.html")
     
-if __name__ == '__main__':
-    app.run(port = 5000) 
 # if __name__ == '__main__':
-#     app.run(debug=True,host = "0.0.0.0")
+#     app.run(port = 5000) 
+if __name__ == '__main__':
+    app.run(debug=True,host = "0.0.0.0")
     
